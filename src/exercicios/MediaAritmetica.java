d2a50c6cc99a17387530ac4dc3de25e0de872a4f
Lucas Reategui <lucasrtgandrade@gmail.com>
2023-08-26 22:05:53 -0300

    Código de Média Aritmética adicionado

--- src/exercicios/MediaAritmetica.java
+++ src/exercicios/MediaAritmetica.java
@@ -1,4 +1,32 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class MediaAritmetica {
+    public static void main (String[] args) {
+        //1. Digite a primeira nota
+        double nota01 = 0;
+        System.out.println("Digite a primeira nota");
+
+        Scanner scanner = new Scanner(System.in);
+        nota01 = scanner.nextDouble();
+
+        //2. Digite a segunda nota
+        double nota02 = 0;
+        System.out.println("Digite a segunda nota");
+        nota02 = scanner.nextDouble();
+
+        //3. Digite a terceira nota
+        double nota03 = 0;
+        System.out.println("Digite a terceira nota");
+        nota03 = scanner.nextDouble();
+
+        scanner.close();
+
+        //4. Calcula as três notas
+        double ma = (nota01 + nota02 + nota03)/3;
+
+        //5. Mostra a média aritmética
+        System.out.println("Suma média é: " + ma);
+    }
 }
