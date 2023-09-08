62791fda5ff8fa38c9f640ddf693c67bacc39329
Lucas Reategui <lucasrtgandrade@gmail.com>
2023-09-07 21:06:49 -0300

    Exercicio 06 - Salário com bônus e auxílio adicionado

--- src/listaDeExercicios02/SalarioComBonus.java
+++ src/listaDeExercicios02/SalarioComBonus.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class SalarioComBonus {
     public static void main(String[] args) {
-
         // Receber salario
         Scanner input = new Scanner(System.in);
         System.out.println("Qual é o seu salário");
