63515d31dc5c4be90cd4e00359468ca5d9e9d91b
Lucas Reategui <lucasrtgandrade@gmail.com>
2023-09-30 14:09:14 -0300

    Lista de ADO - Estrutura de repetição - Exercício 04 - Incremento de 10

---
+++ src/adoDeEstruturaDeRepeticao/Ex04_AdicionaDez.java
@@ -0,0 +1,9 @@
+package adoDeEstruturaDeRepeticao;
+
+public class Ex04_AdicionaDez {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 1000; i += 10) {
+            System.out.println(i);
+        }
+    }
+}
