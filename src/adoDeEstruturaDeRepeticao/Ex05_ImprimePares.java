5902b6b8f45ab70134198ae5a0dd171a0346b654
Lucas Reategui <lucasrtgandrade@gmail.com>
2023-10-01 19:42:40 -0300

    Lista de ADO - Estrutura de repetição - Exercício 05 - Decrementa e imprime pares

---
+++ src/adoDeEstruturaDeRepeticao/Ex05_ImprimePares.java
@@ -0,0 +1,11 @@
+package adoDeEstruturaDeRepeticao;
+
+public class Ex05_ImprimePares {
+    public static void main(String[] args) {
+        for (int i = 100; i > 1; i--) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
