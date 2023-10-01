b57d9cb7f83fb4c4efb6c005e927dd84270f5541
Lucas Reategui <lucasrtgandrade@gmail.com>
2023-10-01 20:16:15 -0300

    Lista de ADO - Estrutura de repetição - Exercício 07 - Multiplos de 7

---
+++ src/adoDeEstruturaDeRepeticao/Ex07_MultiplosDe7.java
@@ -0,0 +1,17 @@
+package adoDeEstruturaDeRepeticao;
+
+public class Ex07_MultiplosDe7 {
+    public static void main(String[] args) {
+        int i = 0;
+
+
+        while (i < 200) {
+
+            if (i % 7 == 0) {
+                System.out.println(i);
+            }
+
+            i = i +1;
+        }
+    }
+}
