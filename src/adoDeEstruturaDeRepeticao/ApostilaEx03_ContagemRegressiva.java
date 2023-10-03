4796f23c5df9ae5626ff5f0b82adc4fd4a5d0f49
Lucas Reategui <lucartgandrade@gmail.com>
2023-10-03 09:40:29 -0300

    Lista de ADO - Estrutura de repetição - Exercício Apostila 03 - Contagem regressiva

---
+++ src/adoDeEstruturaDeRepeticao/ApostilaEx03_ContagemRegressiva.java
@@ -0,0 +1,9 @@
+package adoDeEstruturaDeRepeticao;
+
+public class ApostilaEx03_ContagemRegressiva {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 0; i-=5) {
+            System.out.println(i);
+        }
+    }
+}
